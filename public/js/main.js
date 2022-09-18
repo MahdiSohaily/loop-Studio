7e337f48a9c7986ed8ca71e83aad05ea912144bb
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-18 21:45:06 +0430

    Add script to toggle mobile navigation icon

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,15 @@ const customNav = document.querySelector('.customNav');
 const top_nav = document.querySelector('.top-nav');
 
 btn.addEventListener('click', (event) => {
-    const item = event.target;
-    item.classList.toggle('show');
-    if(item.classList.contains('show')) {
-        customNav.classList.add('open');
-        top_nav.classList.add('mob-nav');
-    }
-})
+  const item = event.target;
+  item.classList.toggle('show');
+  if (item.classList.contains('show')) {
+    item.src = './images/icon-close.svg';
+    customNav.classList.add('open');
+    top_nav.classList.add('mob-nav');
+  } else {
+    item.src = './images/icon-hamburger.svg';
+    customNav.classList.remove('open');
+    top_nav.classList.remove('mob-nav');
+  }
+});
