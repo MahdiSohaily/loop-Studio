5662adbce77d30905a3c8b5a2181037a09fcdebb
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-18 21:34:03 +0430

    Update main navbar styles

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.toggle');
+const customNav = document.getElementById('#customNav');
+const top_nav = document.querySelector('.top-nav');
+
+btn.addEventListener('click', (event) => {
+    console.log(event.target.src)
+})
